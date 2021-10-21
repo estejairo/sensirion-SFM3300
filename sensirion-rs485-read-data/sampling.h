efc9fd14ff5d0e25d6f0926fd1d5f85a4a78830c
jairoegc <jairoegc@gmail.com>
2021-10-21 12:11:04 -0300

    New files for reading hamilton transductor outputs

---
+++ sensirion-rs485-read-data/sampling.h
@@ -0,0 +1,21 @@
+
+#ifndef _SAMPLING_H
+#define _SAMPLING_H
+
+typedef struct {
+  float in_1;
+  float in_2;
+  float in_3;
+  float in_4;
+  float in_5;
+  float in_6;
+  float in_7;
+  float in_8;
+  float in_9;
+  float in_10;
+  float out;
+  float max_samp;
+  } Filter_fir;
+
+
+#endif
